ecb49eadd5b080cd060ebb2847b36f6a237757a6
deV-inh08 <vintd2004@gmail.com>
2024-07-23 15:38:51 +0700

    initial home

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,17 @@
 let getIconDown = document.querySelector(".icon");
-let getLine = document.querySelector(".line");
 
-// Hiển thị navigation
-getIconDown.addEventListener("click", () => {
-    
-});
+
+
+// Click to navigation
+let navMenuItem = () => {
+    let getLine = document.querySelector(".line");
+    let getMenuItem = document.querySelectorAll(".menu__item");
+    getMenuItem.forEach(element => {
+        element.addEventListener("click", (e) => {
+            getLine.style.display = "inline-block"
+            getLine.style.left = `${e.target.offsetLeft}px`
+            getLine.style.width = `${e.target.offsetWidth - 20}px`
+        })
+    });
+}
+navMenuItem()
